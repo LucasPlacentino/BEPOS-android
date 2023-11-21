2edcf73ceb1dd36ca80c2f275b10ed4765ad32df
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-11-21 12:09:35 +0100

    Room SQLite data draft

---
+++ app/src/main/java/be/bepolytech/bepos/Item.kt
@@ -0,0 +1,34 @@
+package be.bepolytech.bepos
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity
+data class Item(
+    @PrimaryKey val id: String,
+    @ColumnInfo(name="name") val name: String?,
+    @ColumnInfo(name="quantity") val quantity: Int = 0,
+    @ColumnInfo(name="price") val price: Float = 0f,
+    @ColumnInfo(name="type") val type: String?
+)
+
+@Entity
+data class Transaction(
+    @PrimaryKey val date: Date,
+    @ColumnInfo(name="item_id") val itemId: String,
+    @ColumnInfo(name="item_name") val itemName: String?,
+    @ColumnInfo(name="quantity") val quantity: Int,
+    @ColumnInfo(name="price") val price: Float,
+
+)
+
+@Entity
+data class Restock(
+    @PrimaryKey val date: Date,
+    @ColumnInfo(name="item_id") val itemId: String,
+    @ColumnInfo(name="quantity") val quantity: Int,
+    @ColumnInfo(name="price") val price: Float
+)
+
